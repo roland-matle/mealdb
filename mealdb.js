346034dd422eb954407050d25f58f7a1554f5f84
Roland <matleroland@gmail.com>
2024-05-13 19:24:29 +0200

    api recept keresés

--- mealdb.js
+++ mealdb.js
@@ -3,12 +3,24 @@ const $container = document.querySelector(".js-receptek")
 const $searchInput = document.querySelector("[name=food]")
 const $errorField = document.querySelector(".js-error")
 
-function formSubmitted(event){
-    event.preventDefault();
-    $errorField.innerHTML = "";
-    const value = $searchInput.value;
-    if (value.length > 0) {
+const API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
 
+function getApiUrl (search) {
+    return API_URL + search
+}
+
+function renderRecipe(response){
+    console.log("render", response);
+}
+
+function formSubmitted(event) {
+    event.preventDefault();
+    const value = $searchInput.value.trim();
+    if (value.length > 0) {
+        $errorField.innerHTML = "";
+        fetch(getApiUrl(value))
+            .then((data) => data.json())
+            .then(renderRecipe);
     } else {
         $errorField.innerHTML = "Hibás keresés: üres keresőkifejezés.";
     }
